b92473fee713b22a12d73d1b0f86e673ebe22606
ninky <0226427@up.edu.mx>
2022-04-28 08:14:01 -0500

    cambio
    cambio

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
             <ion-menu-toggle auto-hide="false" v-for="(p, i) in appPages" :key="i">
               <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
                 <ion-icon slot="start" :ios="p.iosIcon" :md="p.mdIcon"></ion-icon>
-                <ion-label>{{ p.title }}</ion-label>
+                <ion-label>Hola</ion-label>
               </ion-item>
             </ion-menu-toggle>
           </ion-list>
